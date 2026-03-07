b8e56f072a0721c2613ceeab3cade81e63aefb67
Richflexy <Ddrressyo@gmail.com>
2026-03-08 05:15:05 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("8730805668:AAGnUrxv2NGZ0X1tcBoPVOLeTHzOq2PT-mQ
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "🚀 Bot is running 24/7!")
+
+bot.infinity_polling()
