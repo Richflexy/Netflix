d682e42df69d9f5fdad399b5cbd7c94ed7114899
Richflexy <Ddrressyo@gmail.com>
2026-03-08 05:26:38 +0530

    Update bot token and message response

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import telebot
-import os
 
-TOKEN = os.getenv("8730805668:AAGnUrxv2NGZ0X1tcBoPVOLeTHzOq2PT-mQ
+Put your new token inside the quotes
+
+TOKEN = "8730805668:AAGnUrxv2NGZ0X1tcBoPVOLeTHzOq2PT-mQ"
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "🚀 Bot is running 24/7!")
+bot.send_message(message.chat.id, "🚀 Bot is running!")
 
 bot.infinity_polling()
