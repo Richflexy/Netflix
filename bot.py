c3a04f7d7ed1b4683cc6ee5590b7e3e0d4a8d090
Richflexy <Ddrressyo@gmail.com>
2026-03-08 05:33:27 +0530

    Fix formatting and comments in bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import telebot
 
-Put your new token inside the quotes
-
+# Put your token inside quotes
 TOKEN = "8730805668:AAGnUrxv2NGZ0X1tcBoPVOLeTHzOq2PT-mQ"
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-bot.send_message(message.chat.id, "🚀 Bot is running!")
+    bot.send_message(message.chat.id, "🚀 Bot is running!")
 
 bot.infinity_polling()
